126d95d08c34603193ddf88ee6ede24fd3d9910c
wassay-23 <smabdulwassayquadri@gmail.com>
2026-01-25 21:52:24 +0500

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: 'https://drive.google.com/file/d/1hjmM0nedegFmKI8h-Pno_d0xn88RJbcb/view?usp=sharing',
+            href: 'https://drive.google.com/file/d/1xkdXLllZrrhkuPhkWmhASPD2RW0CvtHr/view?usp=sharing',
             style: 'rounded-br-md',
             download: true
         }
